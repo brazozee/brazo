d284fee870df916e1aaf036701fe32618a8cbc6f
Thato <kolobe1945@gmail.com>
2023-05-09 14:40:23 +0200

    first commit

---
+++ src/FirstCommit.java
@@ -0,0 +1,10 @@
+public class FirstCommit {
+
+    public static void main(String[] args){
+
+        int a  = 30;
+        int b = 50;
+        int sum = a + b;
+        System.out.println("The sum of number: " + a + " + " + b + " = " + sum );
+    }
+}
